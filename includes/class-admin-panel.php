691a08f2185dece77705356c12b907d951073ffe
CickandLimited <140349148+CickandLimited@users.noreply.github.com>
2025-06-23 14:07:47 +0100

    Update class-admin-panel.php

--- includes/class-admin-panel.php
+++ includes/class-admin-panel.php
@@ -90,6 +90,7 @@ class Ayotte_Admin_Panel {
        $users = get_users([
     'role' => 'customer',
     'meta_key' => 'ayotte_precourse_token',
+                          ]);
 
         echo '<div class="wrap"><h1>Student Progress</h1><form method="post">';
         wp_nonce_field('ayotte_assign_forms');
